0f8bbe3819b6fb4a32137dca799985980f46f710
s4m0r1 <bhms1270253@gn.iwasaki.ac.jp>
2020-07-27 18:51:59 +0900

    hlpコマンド追加

--- src/main.py
+++ src/main.py
@@ -1,9 +1,9 @@
 import discord
 from discord.ext import commands
 from gtts import gTTS
-import schedule
-import time
+from message import help_mes
 import settings
+import message
 
 # 環境変数の読み出しだけど頭わるわるなのであとで変えておく
 TOKEN = settings.TOKEN
@@ -11,16 +11,15 @@ BOT_HelloWorld = int(settings.BOT_HelloWorld)
 TEST_Channel = settings.TEST_Channel
 AFK = settings.AFK
 GUILD = settings.GUILD
-KIYATAKE = settings.KIYATAKE
 JIHOU = settings.JIHOU
 ROOM1 = settings.ROOM1
+help_mes = message.help_mes
 
 bot = commands.Bot(command_prefix='/jihou ')
 
 global vc
 
-
-
+# 起動
 @bot.event
 async def on_ready():
     print("Login OK!")
@@ -33,36 +32,13 @@ async def find_voice(ctx):
         bot.get_all_channels(), id=ROOM1)
     print(len(channel.members))
 
-# @bot.event
-# async def on_voice_state_update(member, before, after):
-#     # AFKに移動しない
-#     global vc
-#     if member.id == JIHOU or member.id == KIYATAKE:
-#         print("除外")
-#         return
-#     elif vc:
-#         print("移動！")
-#         await vc.move_to(after.channel)
-#     elif after.channel.id == AFK:
-#         return
-#     elif after.channel.id == None:
-#         vc.disconnect()
-#     else:
-#         vc = await after.channel.connect()
-#     # elif vc == None:
-#     # vc = await after.channel.connect()
-#     # else:
-#     # vc = await vc.move_to(after.channel)
-
 # 接続～
-
 @bot.command()
 async def connect(ctx):
     global vc
     vc = await ctx.author.voice.channel.connect()
 
 # コマンド実行者のボイスチャンネルに移動して読む(TTSチャンネル以外)
-
 @bot.command()
 async def ts(ctx, arg):
     global vc
@@ -75,12 +51,20 @@ async def ts(ctx, arg):
         vc.play(discord.FFmpegPCMAudio(
             'a.mp3'))
 
-# 切断
+# Help
+@bot.command()
+async def hlp(ctx):
+    if ctx.channel.id == int(ROOM1):
+        await ctx.send(help_mes)
+    else:
+        return
 
+# 切断
 @bot.command()
 async def discon(ctx):
     global vc
     await vc.disconnect()
+
 
 bot.run(TOKEN)
 
---
+++ src/message.py
@@ -0,0 +1,8 @@
+help_mes = """
+\n```
+/jihou connect  あなたの接続しているボイスチャットに接続
+/jihou discon   ボイスチャットから切断
+/jihou ts       TTSを使用
+/jihou hlp      ヘルプを表示
+```
+"""
